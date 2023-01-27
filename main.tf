ff2195ab1cd2077c00e0483303e2847e9aab45bc
shivandroid-vt <74255503+shivandroid-vt@users.noreply.github.com>
2023-01-27 15:14:55 -0600

    updated main.tf

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
+  
   name    = "blog"
-
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
