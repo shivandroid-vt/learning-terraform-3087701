25142e555e0d33e6c3afc677da3c1a8f2a562ab2
shivandroid-vt <74255503+shivandroid-vt@users.noreply.github.com>
2023-01-27 16:05:44 -0600

    reverting 3

--- main.tf
+++ main.tf
@@ -14,21 +14,8 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+data "aws_vpc" "default" {
+    default = true
 }
 
 
@@ -48,7 +35,7 @@ module "blog_sg" {
   version = "4.17.1"
   name    = "blog"
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
